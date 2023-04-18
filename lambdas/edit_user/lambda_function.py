609e520e0acc34236a59bf8bf813e9678dda31c9
nanason01 <nanason@umich.edu>
2023-04-18 13:29:14 -0400

    impl edit_user

--- lambdas/edit_user/lambda_function.py
+++ lambdas/edit_user/lambda_function.py
@@ -1,11 +1,36 @@
+import json
+
+from common import users
 from common.CORS import CORS
+
+# Expected event format:
+#
+# {
+#     "user_id": "string",
+#     "field": "field_value",
+#     ... (other fields to change) ...
+# }
+#
+# Expected response format if successful:
+#
+# {
+#     "statusCode": 200
+# }
+# Expected response format if user doesn't exist:
+#
+# {
+#     "statusCode": 404
+# }
 
 
 @CORS
 def lambda_handler(event, _):
-    # TODO implement
-
     print(event)
-    return {
-        'statusCode': 501,
-    }
+    user_id = event['pathParameters']['user_id']
+    changed_fields = json.loads(event['body'])
+
+    if not users.user_exists(user_id):
+        return {'statusCode': 404}
+
+    users.update_user(user_id, **changed_fields)
+    return {'statusCode': 200}
