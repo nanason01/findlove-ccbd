d9ff9cfe5a27c585bb8980d10d1be5e3ca1d1cdc
Aryan Jalali <aryanjalali1234@gmail.com>
2023-05-08 04:12:00 -0400

    Made a new lambda get_profiles

---
+++ lambdas/get_profiles/lambda_function.py
@@ -0,0 +1,44 @@
+import simplejson as json
+
+from common import users
+from common.CORS import CORS
+
+# Expected event format:
+#
+# {
+#     "user_id": "string"
+# }
+#
+# Expected response format if successful:
+#
+# {
+#     "statusCode": 200,
+#     "user": {
+#         "field": "field_value",
+#         ... (other fields) ...
+#     }
+# }
+# Expected response format if user doesn't exist:
+#
+# {
+#     "statusCode": 404
+# }
+
+
+@CORS
+def lambda_handler(event, _):
+    user_id = event['pathParameters']['id']
+    print("In get_profiles")
+    if not users.user_exists(user_id):
+        print("In user doesn't exit call")
+        return {'statusCode': 404}
+    
+    print("Get user called")
+    user = users.get_user(user_id)
+    print(user)
+    return {
+        'statusCode': 200,
+        'body': json.dumps({
+            'user': user
+        })
+    }
